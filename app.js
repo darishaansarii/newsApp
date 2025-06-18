1822193b4e149dc0a5af8f0a1e82587797b21e3b
Darisha Ansari <142822592+darishaansarii@users.noreply.github.com>
2025-06-18 11:08:54 +0500

    resolved some API error

--- app.js
+++ app.js
@@ -3,30 +3,35 @@ let input = document.getElementById("inputBox");
 let showData = document.getElementById("showData");
 
 const searchNews = () => {
-  try {
-    showData.innerHTML = `<div class="spinner-grow text-secondary" role="status"><span class="visually-hidden">Loading...</span></div>`;
+    try {
+        showData.innerHTML = `<div class="spinner-grow text-secondary" role="status"><span class="visually-hidden">Loading...</span></div>`;
 
-    let api_url = `https://newsapi.org/v2/top-headlines?q=${input.value}&country=us&apiKey=${api_key}`;
+        let api_url = `https://newsapi.org/v2/top-headlines?q=${input.value}&country=us&apiKey=${api_key}`;
 
-    fetch(api_url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.articles);
+        fetch(api_url)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                showData.innerHTML = "";
 
-        showData.innerHTML = "";
-        data.articles.forEach((e, i) => {
-          showData.innerHTML += `
-            <div class="card d-flex flex-column justify-content-between" style="width: 18rem; height: 100%;">
-              <img src="${e.urlToImage}" class="card-img-top" alt="news-img">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">${e.title}</h5>
-                <p class="card-text" style="flex-grow: 1;">${e.description}</p>
-                <a href="${e.url}" target="_blank" class="btn btn-danger mt-auto">Read More</a>
-              </div>
-            </div>`;
-        });
-      });
-  } catch (err) {
-    console.log(err);
-  }
+                if (data.articles && data.articles.length > 0) {
+                    data.articles.forEach((e, i) => {
+                        showData.innerHTML += `
+                        <div class="card d-flex flex-column justify-content-between" style="width: 18rem; height: 100%;">
+                          <img src="${e.urlToImage}" class="card-img-top" alt="news-img">
+                          <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">${e.title}</h5>
+                            <p class="card-text" style="flex-grow: 1;">${e.description}</p>
+                            <a href="${e.url}" target="_blank" class="btn btn-danger mt-auto">Read More</a>
+                          </div>
+                        </div>`;
+                    });
+                } else {
+                    showData.innerHTML = `<p>No news found or request failed.</p>`;
+                }
+            });
+    } catch (err) {
+        console.log(err);
+        showData.innerHTML = `<p>Something went wrong.</p>`;
+    }
 };
